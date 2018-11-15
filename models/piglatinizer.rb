4756ab8ab0687a559ff1290dc81a2c9dfb57cd2c
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-15 17:24:42 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,13 +1,14 @@
 class PigLatinizer
   def piglatinize(text)
 
-
-
+    if text[0].downcase[/[aeiou]/].count > 0
+      text+='w'
+    else
 
     # nothing special (starts with ONE consonant)
     text = text+text[0]
     text[0] = ''
-
+    end
     # ALL cases
     text+'ay'
   end
