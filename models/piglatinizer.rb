9f68fcb6f444159169e00a1977843a520fcbfb32
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-15 17:26:03 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,10 +4,8 @@ class PigLatinizer
     if text[0].downcase[/[aeiou]/].count > 0
       text+='w'
     else
-
-    # nothing special (starts with ONE consonant)
-    text = text+text[0]
-    text[0] = ''
+      # nothing special (starts with ONE consonant)
+      text = text[1..-1]+text[0]
     end
     # ALL cases
     text+'ay'
