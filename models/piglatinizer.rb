77aabb1c3201e03e46c01414cc9efe4b1475ce56
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-15 17:28:48 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,15 +1,12 @@
 class PigLatinizer
   def piglatinize(word)
-
-    if word[0].downcase[/[aeiou]/] # starts with a vowel
+    if word[0].downcase[/[aeiou]/]
       word+='w'
     else
-
       while !word[0].downcase[/[aeiou]/]
-        word = word[1..-1]+word[0] # starts with 1 consonant
+        word = word[1..-1]+word[0]
       end
     end
-    # ALL cases
     word+'ay'
   end
 end
