f7437b96f34404e073e4222b0c1cb3588b051c88
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-15 17:38:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer'
 
 class App < Sinatra::Base
 
   get '/' do
-    erb :user_input  
+    erb :user_input
+  end
+
+  post 'piglatinize' do
+    @text = params[:user_phrase]
+    PigLatinizer
   end
 
 end
